ca9014992fcb56e7d3abda74984ae53895d1ad75
zuckerburger <131454746+zuckerburger@users.noreply.github.com>
2026-02-02 17:51:33 +1100

    Add main method to run scene example

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
+#include "scene.h"
+#include "camera.h"
+#include "sphere.h"
+#include "renderer.h"
+#include "ray_tracing_engine.h"
 #include "canvas.h"
-#include <cstdint>
+#include <memory>
 
-int main(int argc, char *argv[]) {
+int main () {
+  Scene scene{};
 
-  int width = 1920;
-  int height = 1080;
+  Vec3 sphere_origin = Vec3(0,0,2);
+  float radius = 0.5;
+  auto sphere = std::make_shared<Sphere>(radius, sphere_origin);
+  scene.addPrimitive(sphere);
 
-  Canvas canvas(width, height);
+  auto sphere2 = std::make_shared<Sphere>(radius, Vec3(1,0, 3));
+  auto sphere3 = std::make_shared<Sphere>(radius, Vec3(-1,0, 3));
+  scene.addPrimitive(sphere2);
+  scene.addPrimitive(sphere3);
 
-  for (int i = 0; i < width * height; i++) {
-    uint8_t red = (uint8_t)(((float)i / (width * height)) * 255);
-    Pixel newPixel = {red, uint8_t(255 - red), 255};
-    canvas.setPixelByIndex(newPixel, i);
-  }
+  auto ground = std::make_shared<Sphere>(100, Vec3(0, -100.5, 2));
+  const Vec3 blue(0,0.5,0.5);
+  ground->setMaterial(blue);
+  scene.addPrimitive(ground);
+  Vec3 camera_origin{};
+  Camera camera{camera_origin};
+
+  int focal_length = 1;
+  int image_height = 720;
+  int image_width = image_height * 16.0/9;
+
+  camera.setResolution(image_width, image_height);
+  camera.setFocalLength(focal_length);
+
+  RayTracingEngine raytracer = RayTracingEngine();
+
+  raytracer.addMultiSampling(MSAA_4X);
+
+  Canvas canvas{image_width, image_height};
+  raytracer.render(camera, scene, canvas);
   canvas.createImage();
-
-  // output.close();
   return 0;
 }
